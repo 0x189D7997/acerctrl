a1cae0fe444501289c48d335434b000b7fffab94
0x189D7997 <199489335+0x189D7997@users.noreply.github.com>
2026-08-18 00:36:06 +0000

    Fix typo

--- acerctrld/main.cpp
+++ acerctrld/main.cpp
@@ -210,7 +210,7 @@ namespace acerctrld {
 			while(std::getline(values_file, line)) {
 				handleCommand(line);
 				// delay is needed for subsequent RGB commands to work properly, otherwise the controller may randomly ignore some of them
-				usleep(1650*1000); // 1650ms second, derived experimentally, anything less than 1600ms causes issues
+				usleep(1650*1000); // 1650ms, derived experimentally, anything less than 1600ms causes issues
 			}
 		} else {
 			std::println("[ERR] Failed to open /var/lib/acerctrl/set_values!");
